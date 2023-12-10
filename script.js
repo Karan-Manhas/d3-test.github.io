f3a35e3a0c0aa0779aee79297f50fae5699c211e
kmanhas <km2354@bath.ac.uk>
2023-12-10 14:56:57 +0000

    changes to files

--- script.js
+++ script.js
@@ -7,17 +7,19 @@ const svg = d3.select("#map")
     .attr("width", svgWidth)
     .attr("height", svgHeight);
 
-const projection = d3.geoNaturalEarth1() // Use the Natural Earth projection
+const projection = d3.geoNaturalEarth1()
     .scale(150)
     .translate([svgWidth / 2, svgHeight / 2]);
 
 const path = d3.geoPath().projection(projection);
 
-// Load the world map GeoJSON data
-d3.json("https://raw.githubusercontent.com/d3/d3-geo/master/world-110m.json").then(function (data) {
+// Load the GeoJSON data
+d3.json("map.geojson").then(function (data) {
     svg.selectAll("path")
         .data(data.features)
         .enter()
         .append("path")
-        .attr("d", path);
+        .attr("d", path)
+        .attr("stroke", "black")
+        .attr("fill", "lightgray"); // Customize fill color if needed
 });
